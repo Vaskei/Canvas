87da1a2fc525535815fdb708f4e79991780341b9
Goran F <gfvaskei@gmail.com>
2018-03-30 00:42:28 +0200

    update 2

--- canvas.js
+++ canvas.js
@@ -41,19 +41,47 @@ var c = canvas.getContext("2d");
 //     c.stroke();
 // }
 
+var mouse = {
+	x: undefined,
+	y: undefined
+}
+
+var maxRadius = 40;
+var minRadius = 2;
+
+var colorArray = [
+	'#6C6E58',
+	'#3E423A',
+	'#417378',
+	'#A4CFBE',
+	'#F4F7D9'
+];
+
+window.addEventListener('mousemove', function (event) {
+	mouse.x = event.x;
+	mouse.y = event.y;
+});
+
+window.addEventListener('resize', function () {
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+
+	init();
+});
+
 function Circle(x, y, dx, dy, radius) {
 	this.x = x;
 	this.y = y;
 	this.dx = dx;
 	this.dy = dy;
 	this.radius = radius;
+	this.minRadius = radius;
+	this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
 
 	this.draw = function () {
 		c.beginPath();
 		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-		c.strokeStyle = "blue";
-		c.stroke();
-		c.fillStyle = "aqua";
+		c.fillStyle = this.color;
 		c.fill();
 	};
 
@@ -67,27 +95,53 @@ function Circle(x, y, dx, dy, radius) {
 		this.x += this.dx;
 		this.y += this.dy;
 
+		// interactivity
+		if (mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50) {
+			if (this.radius < maxRadius) {
+				this.radius += 1;
+			}
+		}
+		else if (this.radius > this.minRadius) {
+			this.radius -= 1;
+		}
+
 		this.draw();
 	};
 }
 
 var circleArray = [];
 
-for (var i = 0; i < 200; i++) {
-	var radius = 30; //promjer kruga
-	var x = Math.random() * (innerWidth - radius * 2) + radius; //x koordinata kruga
+for (var i = 0; i < 800; i++) {
+	var radius = Math.random() * 6 + 1; //promjer kruga
+	var x = Math.random() * (innerWidth - radius * 2) + radius; // x koordinata kruga
 	var y = Math.random() * (innerHeight - radius * 2) + radius;
-	var dx = (Math.random() - 0.5); //brzina po x osi
+	var dx = (Math.random() - 0.5); // brzina po x osi
 	var dy = (Math.random() - 0.5);
 
 	circleArray.push(new Circle(x, y, dx, dy, radius));
 }
 
-var x = Math.random() * innerWidth; //x koordinata kruga
+var circleArray = [];
+
+function init() {
+	circleArray = [];
+
+	for (var i = 0; i < 800; i++) {
+		var radius = Math.random() * 6 + 1; //promjer kruga
+		var x = Math.random() * (innerWidth - radius * 2) + radius; // x koordinata kruga
+		var y = Math.random() * (innerHeight - radius * 2) + radius;
+		var dx = (Math.random() - 0.5); // brzina po x osi
+		var dy = (Math.random() - 0.5);
+
+		circleArray.push(new Circle(x, y, dx, dy, radius));
+	}
+}
+
+var x = Math.random() * innerWidth; // x koordinata kruga
 var y = Math.random() * innerHeight;
-var dx = (Math.random() - 0.5); //brzina po x osi
+var dx = (Math.random() - 0.5); // brzina po x osi
 var dy = (Math.random() - 0.5);
-var radius = 30; //promjer kruga
+var radius = 30; // promjer kruga
 
 function animate() {
 	requestAnimationFrame(animate);
@@ -98,4 +152,5 @@ function animate() {
 	}
 }
 
+init();
 animate();
